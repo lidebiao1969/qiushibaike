51d8eb74eb0d285eec0d9273c613ab6fe5be76d0
lidebiao1969 <362886827@qq.com>
2020-10-28 20:13:37 +0800

    ff

--- pages/paper/paper.vue
+++ pages/paper/paper.vue
@@ -1,21 +1,27 @@
 <template>
 	<view class="page">
-		
-		<view class="paper-right-popup-mask">
+		<!-- 操作菜单 -->
+		 <view class="paper-right-popup-mask">
 			<view class="paper-right-popup" v-show="show">
-				<view class="u-f-ac" >
-					<view class="iconfont icon-soushuo-copy" hover-class="paper-right-popup-h"> 添加糗友</view>
+				<view class="u-f-ac"  @tap='addfriend'>
+					<view class="iconfont icon-soushuo-copy"
+					 hover-class="paper-right-popup-h"> 
+					 </view>
+					 添加糗友
 				</view>
-				<view class="u-f-ac">
-					<view class="iconfont icon-qingchu1" hover-class="paper-right-popup-h"> 清除</view>
+				<view class="u-f-ac" @tap="clear">
+					<view class="iconfont icon-qingchu1" 
+					hover-class="paper-right-popup-h" >
+					 </view>
+					 清除缓存
 				</view>
 			</view>
-		</view>
+		</view> 
 		
 		<view v-for="(item,index) in list " :key="index" @click="go(index)" >
 			<paper-list :list="item" ></paper-list>
 		</view>
-		<load-more loadtext="上拉加载更多"></load-more>
+		<load-more :loadtext="loadtext"></load-more>
 	</view>
 </template>
 <script>
@@ -65,15 +71,32 @@
 			}
 			
 		},
+		
 		onPullDownRefresh() {
 			this.getData();
 		},
 		onReachBottom(){
 			this.getD();
 		},
+		
 		methods: {
+			addfriend(){
+				console.log("加好友")
+				this.hidepopup()
+			},
+			clear(){
+				console.log("清除好友")
+				this.hidepopup()
+			},
+			showpopup(){
+				this.show=true
+			},
+			hidepopup(){
+				this.show=false
+			},
 			go(index){
-				console.log(index)
+				console.log(index);
+				this.show=false;
 			},
 			getData(){
 				setTimeout(()=>{
@@ -138,28 +161,47 @@
 					
 		}
 		//点位		
+		},
+		onNavigationBarButtonTap(e) {
+			//console.log(e);
+			if(e.index==1){
+				this.showpopup()
+			} 
 		}
 		
 	}
 </script>
 
-<style>
+<style scoped>
 	.page{
 		margin: 0 20upx;
 		}
-	
+	.u-f-ac{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 	.paper-right-popup{
 		padding: 20upx 20upx;
 		position: fixed;
 		right: 0;
-		top:100upx;
+		
+		/* #ifdef APP-PLUS */
+		top:10upx;
+		/* #endif */
+		/* #ifdef H5 */
+		top:70upx;
+		/* #endif */
 		z-index: 100;
 		width: 250upx;
-		height: 120upx;
-		background-color: #D8D8D8;
+		
+		background-color: #FFFFFF;
 		box-shadow: 1upx 1upx 20upx 2upx #cccccc;
 	}
 	.paper-right-popup-h{
 		background-color: red;
+	}
+	.paper-right-popup>view>view{
+		margin-right: 20upx;
 	}
 </style>
