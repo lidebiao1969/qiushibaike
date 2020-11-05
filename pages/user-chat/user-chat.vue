41dc0be0d8e6765277d1fde6817c87978c1f3249
lidebiao1969 <362886827@qq.com>
2020-11-05 09:42:58 +0800

    ff

--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,32 +1,13 @@
 <template>
 	<view class="page">
 		
-		<block  v-for="(item,index) in list" :key="index">
-		<!-- user-chat-list user-chat-me-->
-			<view style="text-align: center;">{{item.time}}</view>
-			<view class="user-chat-list " :class="item.isme?'user-chat-me':''">
-				  
-				 <image src="../../static/HEAD/mm1.png" mode="widthFix" lazy-load></image> 
-				<view class="user-chat-list-body">
-				 <image v-if="item.type=='img'"  :src="item.imgpic" mode="aspectFit"></image> 
-				 <text  v-if="item.type=='text'">随便喝</text> 
-				</view>
-			</view>
-		</block>
+	
 		<scroll-view  :scroll-top="scrollTop" scroll-y="true" 
 				:scroll-into-view="scrollinto" 
 				:style="{height:swiperHeight+'px'}"
 				@scroll="scroll">
 				<block  v-for="(item,index) in list" :key="index">
-					<view :id="item.id" class="user-chat-list-time">{{item.id}} </view> 
-					<view class="user-chat-list" :class="{'user-chat-me':item.isme}">
-						  <image :src="item.userpic" mode="widthFix" lazy-load></image> 
-						<view class="user-chat-list-body">
-							 <image  v-if="item.type=='img'" :src="item.imgsrc" mode="aspectFit"></image> 
-							 <text v-if="item.type=='text'">{{item.content}}</text> 
-						</view>
-						
-					</view>
+					<user-chat-com :item="item" :index="index"></user-chat-com>
 				</block>
 		 	</scroll-view>
 		
@@ -40,7 +21,7 @@
 </template>
 
 <script>
-	
+	import dateutil from "../../common/util.js"
 	export default {
 		data() {
 			return {
@@ -58,8 +39,11 @@
 		onLoad() {
 			// this.timeStamp();
 			this.loadData();
-		
-				this.scrollinto='ss'+this.list.length;  
+		this.$nextTick(function(){
+			this.scrollinto='ss'+this.list.length; 
+			console.log(this.scrollinto)
+		})
+				 
 			
 			   
 			uni.getSystemInfo({
@@ -147,7 +131,7 @@
 					content:"随便喝3333",
 					type:"text",//img
 					imgsrc:"../../static/HEAD/man.png",
-					time:"1584592912",
+					time:"1604376672",
 					id:'ss5'
 				},{
 					isme:false,
@@ -155,13 +139,16 @@
 					
 					type:"img",//img
 					imgsrc:"../../static/HEAD/man.png",
-					time:"1584593712",
+					time:"1604321072",
 					id:'ss6'
 				}];
 				
 				var arr=list;
 				arr.forEach((v,i)=>{
-					v.time=this.getLocalTime(v.time);
+					//v.time=this.getLocalTime(v.time);
+					var a=dateutil.dateUtils.timestampToTime(v.time);
+					
+					v.time=dateutil.dateUtils.format(a);
 					
 				})
 				this.list=arr;
@@ -174,17 +161,21 @@
 					userpic:"../../static/HEAD/mm1.png",
 					content:"随便喝88",
 					type:"text",//img
-					time:"1604315271",
+					time:'',
 					id:''
 			}
-			obj.time=this.getLocalTime(obj.time)
+			//obj.time=this.getLocalTime(obj.time)
+			obj.time=new Date().getTime();
+			var a=dateutil.dateUtils.timestampToTime(obj.time);
+			obj.time=dateutil.dateUtils.format(a);
+			
 			obj.id='ss'+(this.list.length +1);
 			 obj.content=t;
 			// console.log(JSON.stringify(obj))
 			this.list.push(obj);
 			this.$nextTick(function(){
 			this.scrollinto='ss'+(this.list.length);
-			console.log(this.scrollinto)
+			//console.log(this.scrollinto)
 			})
 			
 			
@@ -197,80 +188,6 @@
 	.page{
 		padding:20upx;
 	}
-	
-.user-chat-list-time{
-		padding-top: 30upx ;
-		font-size: 25upx;
-		color: #333333;
-		text-align: center;
-	}
-.user-chat-list{
-	display: flex;
-	margin: 20upx 0;
-	
-	
-}
-
-.user-chat-list>image{
-	width: 100upx;
-	height: 100upx;
-	flex-shrink: 0;
-	margin-right: 30upx;
-	border-radius: 100%;
-	
-}
-.user-chat-list-body{
-	background: #F4F4F4;
-	padding: 25upx;
-	flex: 1;
-	height: 100upx;	
-	border-radius: 20upx;
-	position: relative;
-	margin-right: 120upx;
-	display: flex;
-	justify-content:flex-start;
-}
-
-.user-chat-list-body>image{
-	width: 100upx;
-	height: 100upx;
-}
-
-
-.user-chat-list-body:after{
-	position: absolute;
-	left: -30upx;
-	right: 0;
-	top:30upx;
-	content: '';
-	width: 0;
-	height: 0;
-	border: 16upx solid #F4F4F4;
-	border-color: transparent #F4F4F4 transparent transparent;
-}
-.user-chat-me{
-	display: flex;
-	flex-direction: row-reverse;
-}
-.user-chat-me>image{
-	/* width: 100upx; */
-	height: 100upx;
-	flex-shrink: 0;
-	margin-right: 30upx;
-	border-radius: 100%;
-	
-}
-.user-chat-me .user-chat-list-body{
-	margin-right: 20upx;
-	margin-left: 120upx;
-	justify-content:flex-end;
-}
-.user-chat-me .user-chat-list-body:after{
-	left: auto;
-	right: -30upx;
-	border-color: transparent transparent transparent #F4F4F4;
-	
-}
 
 
 
